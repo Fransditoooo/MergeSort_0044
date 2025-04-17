d7b85369f4b91c850d1dacbc10de0416c1d39335
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-04-17 11:03:26 +0700

    Masukkan Array index ke -

---
+++ mergeSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input() {
+            while (true){
+                cout << "Masukkan panjang element array: ";
+                cin >> n;
+
+                if (n<= 20) {
+                    break;
+                }
+                else {
+                    cout << "\nMaksimal panjang array adalah 20";
+                }
+            }
+
+            cout << "\n===============";
+            cout << "\nInputkan Isi Element Array";
+            cout << "\n===============" << endl;
+
+            for (int i = 0; i < n; i++){
+                cout << "Masukkan Array index ke - " << i << ":";
+                cin >> arr[i];
+            }
+
+            
+
+
+
+
+}
